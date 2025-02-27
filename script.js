637cc718180d3701e0cd4f2289c52112c9c19b6a
clap0000 <eric6842597@gmail.com>
2025-02-27 19:08:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
     alert("歡迎來到高級便女培育學院！\n這裡的一切都只是為了我的性癖而生，請勿上網上線！");
 });
+
+let index = 0;
+const slides = document.querySelectorAll(".slides a");
+
+function changeSlide() {
+    index = (index + 1) % slides.length;
+    document.querySelector(".slides").style.transform = `translateX(-${index * 100}%)`;
+}
+
+setInterval(changeSlide, 3000); // 每 3 秒換一張
